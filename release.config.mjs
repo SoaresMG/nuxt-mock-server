37330b2477bb9328805795ccdce5ec42399aa891
Leandro Soares <soares.mgleandro@gmail.com>
2024-08-07 21:28:19 +0100

    ci: add changelog and update package.json

--- release.config.mjs
+++ release.config.mjs
@@ -11,7 +11,17 @@ export default {
       },
     ],
     "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
     "@semantic-release/npm",
+    ["@semantic-release/git", {
+      assets: ["docs", "package.json"],
+      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    }],
     "@semantic-release/github",
   ],
 };
