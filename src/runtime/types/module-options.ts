9ce7fa59f7f916be5971324ff16971b57a9ab79e
Leandro Soares <soares.mgleandro@gmail.com>
2024-08-12 14:15:38 +0100

    chore: add moduleOptions property descriptions

--- src/runtime/types/module-options.ts
+++ src/runtime/types/module-options.ts
@@ -1,11 +1,53 @@
 import type { ModulePackageInfo } from "./module-meta";
 
 export interface ModuleOptions {
+  /**
+   * Enables nuxt-mock-server as a whole.
+   *
+   * @default false
+   */
   enabled?: boolean;
+  /**
+   * RegEx to match route paths in Auto mode.
+   * This will not be used for Selective mode endpoints.
+   *
+   * @default "^\\/api\\/.*$"
+   */
   pathMatch?: string;
+  /**
+   * Directory to save the mock presets and files.
+   *
+   * @default "./mocks"
+   */
   mockDir?: string;
+  /**
+   * Enables devtools tab "Mock Server".
+   *
+   * @default true
+   */
   devtools?: boolean;
+  /**
+   * Default preset if no cookie is present.
+   * Devtools will always be disabled when Nuxt is not in dev mode.
+   *
+   * @default "default"
+   */
   preset?: string;
+  /**
+   * @private
+   */
   package?: ModulePackageInfo;
+  /**
+   * Enables automatic mode, otherwise the app will be solely in selective mode.
+   *
+   * @default true
+   */
   auto?: boolean;
+  /**
+   * Routes to generate on module load.
+   * Mock Server will automatically call these routes to generate mock files inside the preset directory specified in the property `preset`.
+   *
+   * @default false
+   */
+  routes?: string[];
 }
